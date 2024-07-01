77eb58174583914ac1986a6cefea89aa43bd8399
Rotha SAMON <samonrotha@gmail.com>
2024-07-01 18:58:34 +0700

    modify the link path to deloy

--- src/js/data/sidebar-data.js
+++ src/js/data/sidebar-data.js
@@ -4,7 +4,7 @@ export const sidebarData = [
         classIcon: '',
         icon: "",
         class : "",
-        img: "/src/images/dashboard.png",
+        img: "./src/images/dashboard.png",
         title: "Dashboard",
         link: "/index.html"
     },
@@ -13,7 +13,7 @@ export const sidebarData = [
         classIcon: '',
         icon: "",
         class : "",
-        img: "/src/images/folder.png",
+        img: "./src/images/folder.png",
         title: "Create Todo List",
         link: "/public/create.html"
     },
@@ -21,7 +21,7 @@ export const sidebarData = [
         classIcon: '',
         icon: "",
         class : "",
-        img: "/src/images/calendar.png",
+        img: "./src/images/calendar.png",
         title: "View and Edit",
         link: "/public/diary-list.html"
     },
@@ -29,7 +29,7 @@ export const sidebarData = [
         classIcon: 'class="toggle-icon"',
         icon: '&#9660;',
         class : "dropdown-header",
-        img: "/src/images/question.png",
+        img: "./src/images/question.png",
         title: "Help",
         link: "#"
     },
